47c4ae64adf943a7178c6617690c36edf45fc8ef
Davi Suga <daviciencia1@gmail.com>
2022-04-24 16:11:26 -0300

    file utility module

--- lib/filter.ml
+++ lib/filter.ml
@@ -4,3 +4,19 @@ module String = struct
   let joinList separator arr = List.fold_left (catStringWith separator) "" arr
   let replace input output = Str.global_replace (Str.regexp_string input) output
 end
+
+module File = struct
+  let read filename callback =
+    let lines = ref [] in
+    let chan = open_in filename in
+    try
+      while true do
+        let new_line = input_line chan in
+        let _ = callback new_line in
+        lines := new_line :: !lines
+      done;
+      !lines
+    with End_of_file ->
+      close_in chan;
+      List.rev !lines
+end
