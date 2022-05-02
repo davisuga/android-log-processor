c7aafdf0cd328194e2294919a49e2418f6e4d9d8
Davi Suga <daviciencia1@gmail.com>
2022-05-02 10:46:39 -0300

    commented code removal

--- lib/filter.ml
+++ lib/filter.ml
@@ -1,12 +1,4 @@
 open Stdio
-(* module String = struct
-     include String
-
-     let catStringWith separator strA strB = strA ^ separator ^ strB
-     let joinArray separator arr = Array.fold_left (catStringWith separator) "" arr
-     let joinList separator arr = List.fold_left (catStringWith separator) "" arr
-     let replace input output = Str.global_replace (Str.regexp_string input) output
-    end *)
 
 let is_not_empty str = str != ""
 
